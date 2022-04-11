27a098647cb2c7a9a48cdaa259a447c5995a9d63
amandaflorian <a.florian@outlook.com.br>
2022-04-11 20:27:17 -0300

    Update Long Polling

---
+++ polling.js
@@ -0,0 +1,25 @@
+function update() {
+    $.ajax({
+        url: '/data-update',
+        success:  function(data) {
+            $('#mudarData').text(data.date);
+            $('#content').text(data.content);
+            update();
+        },
+        timeout: 500000 
+    });
+}
+
+function load() {
+    $.ajax({
+        url: '/data',
+        success: function(data) {
+            $('#content').text(data.content);
+            update();
+        }
+    });
+}
+
+$(document).ready(function() {
+    load();
+});
---
+++ server.py
@@ -0,0 +1,49 @@
+from flask import Flask
+from flask import render_template
+import flask_restful as restful
+
+import os
+import time
+from datetime import datetime
+
+
+app = Flask(__name__)
+api = restful.Api(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+class DataUpdate(restful.Resource):
+
+    def _is_updated(self, request_time):
+        return os.stat('data.txt').st_mtime > request_time
+
+    def get(self):
+        request_time = time.time()
+        while not self._is_updated(request_time):
+            time.sleep(0.5)
+        content = ''
+        with open('data.txt') as data:
+            content = data.read()
+        return {'content': content,
+                'date': datetime.now().strftime('%Y/%m/%d %H:%M:%S')}
+
+
+class Data(restful.Resource):
+
+    def get(self):
+        content = ''
+        with open('data.txt') as data:
+            content = data.read()
+        return {'content': content}
+
+
+api.add_resource(DataUpdate, '/data-update')
+api.add_resource(Data, '/data')
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
